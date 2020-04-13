49427917d5190cc85121c705284627e95717dc5c
ashwinspg <ashwinspg@gmail.com>
2020-04-13 09:55:02 +0530

    removing unwanted code blocks

--- services/movies.go
+++ services/movies.go
@@ -28,25 +28,25 @@ func NewMovie(db *sql.DB, l *logrus.Entry) *Movie {
 //GetMovie - get movie based on uuid
 func (m *Movie) GetMovie(uuid string) (movieDTO dtos.Movie, err error) {
 	movieDTO, err = m.dao.FindByID(uuid)
-	switch err {
-	case nil:
+	if err == nil {
 		return
-	case daos.ErrMovieNotFound:
-		movieDTO, err = m.getMovieFromMovieBuff(uuid)
-		if err != nil {
-			m.l.WithError(err).Errorln("Failed to get Movie Information from MovieBuff")
-			return
-		}
-
-		err = m.dao.Save(movieDTO)
-		if err != nil {
-			m.l.WithError(err).Errorln("Failed to save Movie Information from MovieBuff")
-		}
-		return
-	default:
+	} else if err != daos.ErrMovieNotFound {
 		m.l.WithError(err).Errorln("Failed to get Movie Information from Database")
 		return
 	}
+
+	movieDTO, err = m.getMovieFromMovieBuff(uuid)
+	if err != nil {
+		m.l.WithError(err).Errorln("Failed to get Movie Information from MovieBuff")
+		return
+	}
+
+	err = m.dao.Save(movieDTO)
+	if err != nil {
+		m.l.WithError(err).Errorln("Failed to save Movie Information from MovieBuff")
+	}
+
+	return
 }
 
 func (m *Movie) getMovieFromMovieBuff(uuid string) (movieDTO dtos.Movie, err error) {
